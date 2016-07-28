5d377d6b6cefb569fca490c3a372613ac2bb6006
limeforadime <Battery641212063@hotmail.com>
2016-07-28 00:15:06 -0700

    testing gitignore again

--- src/engine/Board.java
+++ src/engine/Board.java
@@ -130,7 +130,7 @@ public class Board extends JPanel implements ActionListener {
         
         TimerTask update = new UpdateBoard(this); // create new task, which uses this current board as parameter
         
-        timer2.scheduleAtFixedRate( update , 18 , 18); // fire task every 15 ms
+        timer2.scheduleAtFixedRate( update , 1 , 15); // fire task every 15 ms
         
         //updateBoard();
     }

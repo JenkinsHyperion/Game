5797a2b74316c262921f921ccdb6dc122df8adf9
Matt <jenkinsjunkbox@gmail.com>
2016-08-18 23:07:47 -0700

    Revert "properties"
    This reverts commit e281df87baa16a254e274a92b8d71eea379250e5.

--- src/Editing/PropertiesList.java
+++ src/Editing/PropertiesList.java
@@ -1,29 +1,9 @@
 package Editing;
 
-import java.util.ArrayList;
-
 public class PropertiesList {
 
-	private final ArrayList EntityStaticProperties = null; // make array list
 	private Property[] properties;
 	
-	public PropertiesList() {
-		initProperties();
-	}
-
-	/**
-	 * 
-	 * @param type
-	 */
-	public Property[] getPropertiesOfType(String type) {
-		if (type == "EntityStatic"){
-			return EntityStaticProperties;
-		}
-		else return null;
-	}
-	
-	private void initProperties() {
-
-	}
+	//Make your methods to handle passing the individual properties to the editor
 	
 }

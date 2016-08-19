e281df87baa16a254e274a92b8d71eea379250e5
Matt <jenkinsjunkbox@gmail.com>
2016-08-18 23:05:29 -0700

    properties

--- src/Editing/PropertiesList.java
+++ src/Editing/PropertiesList.java
@@ -1,9 +1,29 @@
 package Editing;
 
+import java.util.ArrayList;
+
 public class PropertiesList {
 
+	private final ArrayList EntityStaticProperties = null; // make array list
 	private Property[] properties;
 	
-	//Make your methods to handle passing the individual properties to the editor
+	public PropertiesList() {
+		initProperties();
+	}
+
+	/**
+	 * 
+	 * @param type
+	 */
+	public Property[] getPropertiesOfType(String type) {
+		if (type == "EntityStatic"){
+			return EntityStaticProperties;
+		}
+		else return null;
+	}
+	
+	private void initProperties() {
+
+	}
 	
 }

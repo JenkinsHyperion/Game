aab09991477fa01a40613c8da515b96257510d8a
Matt <jenkinsjunkbox@gmail.com>
2016-09-19 17:56:11 -0700

    fixing editing test

---
+++ src/editing/PropertiesPanel.java
@@ -0,0 +1,215 @@
+package editing;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import editing.*;
+
+
+//HUGE NOTE:
+//I WILL BE CHANGING THIS INTO A PANEL, MAKING IT EXTEND EDITORPANEL.JAVA TO OBTAIN ALL ITS FIELDS,
+//AND PLACING THIS PANEL AT THE BOTTOM OF EDITORPANEL. IT WILL BE COLLAPSED WHEN NOTHING IS SELECTED.
+public class PropertiesPanel extends JPanel implements ActionListener,ItemListener{
+	//create some components
+	JComboBox<String> propertiesListComboBox;
+	JTextField xPosTextField;
+	JTextField yPosTextField;
+	JTextField entNameTextField;
+	JPanel cardsPanel; // this will be the CardLayout panel holder
+	// this will show the current state of the property, 
+	//and will update whenever the property is changed.
+	JLabel collisionStatusLabelcursed;
+	JLabel collisionStatusLabel;
+	JLabel xPosStatusLabel; //testing out this displaying in the radioPanel
+	JLabel yPosStatusLabel;
+	JLabel entNameStatusLabel;
+	//figure out a temporary plan for entTypeStatus later, since it won't be editable
+	//you don't need to change it here.
+	
+	//the panels
+	JPanel radioPanel;
+	JPanel xPosPanel;
+	JPanel yPosPanel;
+	JPanel entityNamePanel;
+	JPanel entityTypePanel;
+	EditorPanel ep;
+	
+	public PropertiesPanel(EditorPanel editorPanelRef) { 		 //constructor 
+		this.ep = editorPanelRef;
+		//setPreferredSize(ep.propPanelDefaultSize);
+		collisionStatusLabelcursed = new JLabel("N/A"); //initializing these so they won't cause null problems
+		collisionStatusLabel = new JLabel("cursed?");
+		xPosStatusLabel = new JLabel("N/A");
+		yPosStatusLabel = new JLabel("N/A");
+		entNameStatusLabel = new JLabel("N/A");
+		
+		radioPanel = createRadioPanel();
+		xPosPanel = createXPosPanel();
+		yPosPanel = createYPosPanel();
+		entityNamePanel = createEntNamePanel();
+		entityTypePanel = createEntTypePanel();
+		//setBackground(Color.GRAY);
+		add(createMainPropertiesPanel());
+	}
+	//this panel will act as the content pane for PropertiesFrame
+	protected JPanel createMainPropertiesPanel() {
+		
+		//int thisPropertyType;   // will be of type Property.STATE, Property.POS, or Property.TEXT, etc.
+		//mainPropertiesPanel will be the main panel inside of PropertiesFrame
+		JPanel mainPropertiesPanel = new JPanel();
+
+		//initialize drop down box for list of Properties
+		propertiesListComboBox = new JComboBox<>(ep.listOfPropLists.get(ep.currentEntIndex).getPropertiesAsString());
+		propertiesListComboBox.setEditable(false);
+		propertiesListComboBox.setSelectedIndex(0);
+		propertiesListComboBox.addItemListener(this);
+		//initialize currentStatusLabel
+
+		//make a panel to hold the combobox
+		JPanel comboBoxPanel = new JPanel();
+		comboBoxPanel.add(propertiesListComboBox);
+		
+		//create the tabs for each type of property to be set (radio, text, etc.)
+		//would create them here, but I already made functions for them:
+		// "createRadioPanel(), createTextPanel(), etc.
+		
+		
+		cardsPanel = new JPanel(new CardLayout()); 
+		cardsPanel.add(radioPanel, propertiesListComboBox.getItemAt(0));
+		cardsPanel.add(xPosPanel, propertiesListComboBox.getItemAt(1));
+		cardsPanel.add(yPosPanel, propertiesListComboBox.getItemAt(2));
+		cardsPanel.add(entityNamePanel, propertiesListComboBox.getItemAt(3));
+		cardsPanel.add(entityTypePanel, propertiesListComboBox.getItemAt(4));
+		mainPropertiesPanel.add(comboBoxPanel, BorderLayout.PAGE_START);
+		mainPropertiesPanel.add(cardsPanel, BorderLayout.CENTER);
+		mainPropertiesPanel.setPreferredSize(new Dimension(200,125));
+		//mainPropertiesPanel.setBackground(Color.GRAY);
+		return mainPropertiesPanel;
+	} // end of createMainPropertiesPanel()
+	
+	public void itemStateChanged(ItemEvent e) {
+		CardLayout cl = (CardLayout)(cardsPanel.getLayout());
+		cl.show(cardsPanel, (String)e.getItem());
+		//updateCurrentStatusLabel(propertiesListComboBox.getSelectedIndex());
+	}
+
+	//this will handle the radio buttons and text fields
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+		//for the radio button panels
+		if (command.equals("COL_TRUE")) {
+			ep.getThisProperty(Property.COL_STATE).setEntityCollidableState(ep.getSelectedEntity(), true);
+			updateCurrentStatusLabel(Property.COL_STATE);
+		}
+		else if(command.equals("COL_FALSE")) {
+			ep.getThisProperty(Property.COL_STATE).setEntityCollidableState(ep.getSelectedEntity(), false);
+			updateCurrentStatusLabel(Property.COL_STATE);
+		}
+		else if(e.getSource() == xPosTextField){
+			/*
+			 * Here, set the xPosition field of the current propertyList's xPos property */
+			ep.getThisProperty(Property.XPOS).setEntityXpos(ep.getSelectedEntity(), Integer.parseInt(command));
+			updateCurrentStatusLabel(Property.XPOS);
+		}
+		else if(e.getSource() == yPosTextField){
+			/*
+			 * Here, set the yPosition field of the current propertyList's yPos property */
+			ep.getThisProperty(Property.YPOS).setEntityYpos(ep.getSelectedEntity(), Integer.parseInt(command));
+			updateCurrentStatusLabel(Property.YPOS);
+		}
+		else if(e.getSource() == entNameTextField){
+			/*
+			 * Here, set the name field of the current propertyList's yPos property */
+			ep.getThisProperty(Property.ENTNAME).setEntityName(ep.getSelectedEntity(), command);
+			updateCurrentStatusLabel(Property.ENTNAME);
+			ep.updateAllEntitiesComboBox();
+		}
+		/*
+		invalidate();
+		validate();
+		repaint(); */
+	}
+	protected void updateCurrentStatusLabel(int propType) {
+		if (propType == Property.COL_STATE) {
+			System.out.println("was able to reach upDateCurrentStatusLabel. Attempting to set collisionStatusLabel again...");
+			collisionStatusLabel.setText(String.valueOf(ep.getThisProperty(propType).getEntityCollidableState(ep.getSelectedEntity())) );
+		}
+		else if (propType == Property.XPOS)
+			xPosStatusLabel.setText(Integer.toString(ep.getThisProperty(propType).getEntityXpos(ep.getSelectedEntity())) );
+		else if (propType == Property.YPOS)
+			yPosStatusLabel.setText(Integer.toString(ep.getThisProperty(propType).getEntityYpos(ep.getSelectedEntity())) );
+		else if (propType == Property.ENTNAME)
+			entNameStatusLabel.setText(ep.getThisProperty(propType).getEntityName(ep.getSelectedEntity()) );
+	}
+// ###### Panel creation section  ###########
+	// will create true/false (radiobutton), text field, and others here:
+	protected JPanel createRadioPanel(){
+		JPanel radioPanel = new JPanel(new BorderLayout());
+		ButtonGroup buttonGroup = new ButtonGroup();			
+		JRadioButton rbTrue = new JRadioButton("True");
+		JRadioButton rbFalse = new JRadioButton("False");
+		buttonGroup.add(rbTrue);
+		buttonGroup.add(rbFalse);
+		rbTrue.setActionCommand("COL_TRUE");
+		rbTrue.addActionListener(this);		
+		rbFalse.setActionCommand("COL_FALSE");
+		rbFalse.addActionListener(this);	
+		//initial check to correctly choose true or false for property
+		if (ep.getThisProperty(Property.COL_STATE).getEntityCollidableState(ep.getSelectedEntity()) == true) {
+			rbTrue.setSelected(true);
+			updateCurrentStatusLabel(Property.COL_STATE);
+		}
+		else {
+			rbFalse.setSelected(true);
+			updateCurrentStatusLabel(Property.COL_STATE);
+		}
+		JPanel labelPanel = new JPanel(new BorderLayout());
+		labelPanel.add(collisionStatusLabel);
+		radioPanel.add(rbTrue, BorderLayout.WEST);
+		radioPanel.add(rbFalse, BorderLayout.CENTER);
+		radioPanel.add(labelPanel, BorderLayout.PAGE_END); 
+		return radioPanel;
+	}
+	protected JPanel createXPosPanel(){
+		updateCurrentStatusLabel(Property.XPOS);
+		JPanel xPosPanel = new JPanel(new BorderLayout());
+		xPosTextField = new JTextField(10);
+		xPosTextField.addActionListener(this);
+		JPanel labelPanel = new JPanel();
+		labelPanel.add(xPosStatusLabel);
+		xPosPanel.add(xPosTextField,BorderLayout.NORTH);
+		xPosPanel.add(labelPanel, BorderLayout.PAGE_END);
+		return xPosPanel;
+	}
+	protected JPanel createYPosPanel(){
+		updateCurrentStatusLabel(Property.YPOS);
+		JPanel yPosPanel = new JPanel(new BorderLayout());
+		yPosTextField = new JTextField(10);
+		yPosTextField.addActionListener(this);
+		JPanel labelPanel = new JPanel();
+		labelPanel.add(yPosStatusLabel);
+		yPosPanel.add(yPosTextField, BorderLayout.NORTH);
+		yPosPanel.add(labelPanel, BorderLayout.PAGE_END);
+		return yPosPanel;
+	}
+	protected JPanel createEntNamePanel(){
+		updateCurrentStatusLabel(Property.ENTNAME);
+		JPanel entNamePanel = new JPanel(new BorderLayout());
+		entNameTextField = new JTextField(15);
+		entNameTextField.addActionListener(this);
+		JPanel labelPanel = new JPanel();
+		labelPanel.add(entNameStatusLabel);
+		entNamePanel.add(entNameTextField, BorderLayout.NORTH);
+		entNamePanel.add(labelPanel, BorderLayout.PAGE_END);
+		return entNamePanel;
+	}
+	protected JPanel createEntTypePanel(){
+		JPanel entTypePanel = new JPanel();
+		collisionStatusLabelcursed = new JLabel();
+		String entTypeString = ep.getThisProperty(Property.ENTTYPE).getEntityType(ep.getSelectedEntity());
+		entTypePanel.add(new JLabel(entTypeString), BorderLayout.PAGE_END);
+		return entTypePanel;
+	}
+} // end of PropertiesPanel class
---
+++ src/editing/Property.java
@@ -0,0 +1,146 @@
+package editing;
+
+import engine.MainWindow;
+import entities.*;
+
+public class Property {
+
+	/* This class determines what variables and methods can be accessed by the editor and under what circumstances.
+	 * To be put into properties list, probably another class 'Properties' that will be made for each type of entity.
+	 * I'm not sure where this "properties" object would go, definitely not in each instance of every entity since it belongs
+	 * to the class as a whole, not individual objects. 
+	 * It might actually just go in the editor system.
+	 */
+	
+	//private String propertyType;
+	private final String entityType;
+	private int propertyType;
+	private String propertyName;
+	private String entityName;
+	private int xpos;
+	private int ypos;
+	private boolean collidable;
+	
+	//IMPORTANT: MAKE AN UPDATER FUNCTION TO SET EACH OF THIS PROPERTY'S COPIES
+	// OF THEIR PROPERTY VARIABLES. THIS UPDATER WILL BE CALLED EVERY TIME A SET() METHOD RUNS.
+	// MAY BE COMPLICATED WHEN THE SLIDERS ARE USED, KEEP THIS IN MIND
+	
+	//TO DO: 
+	// Make the PropertiesList object permanent while the editor is open
+	// and populate it by scanning through every entity,
+	// rather than it only being alive while the current entity's window is open.
+	
+	//set some final constants just like Swing components do.
+	//these constants indicate what kind of property this instance is.
+	public final static int COL_STATE = 0; //collidable state
+	public final static int XPOS = 1;
+	public final static int YPOS = 2;
+	public final static int ENTNAME = 3;
+	public final static int ENTTYPE = 4;
+	
+	
+
+	//I'm leaving Class<> in case something other than entity needs properties, otherwise just Entites get properties for now
+	/**
+	 * 
+	 * @param ent the current selected entity
+	 * @param propType must choose Property.BOOLEAN, Property.POS, or Property.TEXT
+	 */
+	public Property(Entity ent, int propType, String propName) {
+			entityType = ent.getClass().getSimpleName();
+			this.propertyName = propName;
+			if (propType == COL_STATE){
+				this.propertyType = Property.COL_STATE;
+				this.collidable = ent.isCollidable();
+			}
+			else if (propType == XPOS){
+				this.propertyType = Property.XPOS;
+				this.xpos = ent.getX();
+			}
+			else if (propType == YPOS){
+				this.propertyType = Property.YPOS;
+				this.ypos = ent.getY();
+			}
+			else if (propType == ENTNAME) {
+				this.propertyType = Property.ENTNAME;
+				this.entityName = ent.name;
+			}
+			else if (propType == ENTTYPE) {
+				this.propertyType = Property.ENTTYPE;
+			}
+			else {
+				throw new ArithmeticException("Did not enter one of the three property type codes");
+			}
+			/*
+			this.collidable = owner.isCollidable();
+			this.xpos = owner.getX();
+			this.ypos = owner.getY();
+			this.entityName = owner.name;
+			this.entityType = getEntityType(owner);
+			//Integer.class.isInstance(this.xpos);
+			*/
+		/* I didn't even know this was possible and you or me should probably check if theres a better way to set this up.
+		 *  But as I see it each property should go through the get and set methods of the entity, especially the set method 
+		 *  to avoid the Editor being able to give invalid values. Maybe the property object can hold additional parameters like 
+		 *  what value range is allowed 
+		 *  
+		 */
+		
+	}
+	public int getPropertyType(){
+		return this.propertyType;
+	}
+	public String getPropertyName(){
+		return this.propertyName;
+	}
+	public String getEntityType(Entity ent) {
+		return ent.getClass().getSimpleName();		
+	}
+	public String getEntityName(Entity ent) {
+		return ent.name;
+	}
+	public int getEntityXpos(Entity ent){
+		return ent.getX();
+	}
+	public int getEntityYpos(Entity ent){
+		return ent.getY();
+	}
+	public boolean getEntityCollidableState(Entity ent) {
+		return ent.isCollidable();
+	}
+	
+	
+	//(update: ignore this block) 
+	// #### SETTERS #####
+	//	If one of these properties is set, (currently from the PropertiesList class), 
+	//	 then it will set this property's type to that type of value. This works because
+	// 	 there should only be one current property held by each property object.
+	// -These types will be used in the GUI drawing classes to signify what type of window to show.
+	//	 (eg. "pos" for position, will have sliders, "bool" for true/false radio buttons, etc.
+	public void setEntityName(Entity ent, String name) {
+		ent.name = name;
+		this.entityName = name;
+		//propertyType = "text";
+	}
+	/* Propbably won't use this one
+	public void setPropertyName( String name) {
+		this.propertyName = name;
+		//propertyType = "text";
+	} */
+	public void setEntityXpos(Entity ent, int x){
+		ent.setX(x);
+		this.xpos = x;	
+	}
+	public void setEntityYpos(Entity ent, int y){
+		ent.setY(y);
+		this.ypos = y;
+	}
+	public void setEntityCollidableState(Entity ent, boolean state) {
+		ent.setCollidable(state);
+		this.collidable = state;
+	}
+	public String toString() {
+		return this.propertyName;
+	}
+
+}
